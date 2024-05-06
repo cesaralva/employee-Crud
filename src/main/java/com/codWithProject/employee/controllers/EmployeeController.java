33faa3c0a57cf21bb0ab94b3bac7efc13f8f628b
CesarAlva <66078322+cesaralva@users.noreply.github.com>
2024-05-06 14:16:40 -0500

    Terminado

--- src/main/java/com/codWithProject/employee/controllers/EmployeeController.java
+++ src/main/java/com/codWithProject/employee/controllers/EmployeeController.java
@@ -3,20 +3,45 @@ package com.codWithProject.employee.controllers;
 import com.codWithProject.employee.entity.Employee;
 import com.codWithProject.employee.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
 
-    @PostMapping("/create")
-    public Employee postEmployee(@RequestBody Employee employee){
+    @PostMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Employee postEmployee(@RequestBody Employee employee) {
         return employeeService.postEmployee(employee);
+    }
+
+    @GetMapping("list")
+    public List<Employee> getAllEmployee() {
+        return employeeService.getAllEmployee();
+    }
+
+    @GetMapping("/{id}")
+    public Employee update(@PathVariable Long id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployee(id);
+    }
+
+    ;
+
+    @PutMapping("/{id}")
+    public void updateEmployee(@RequestBody Employee employee, @PathVariable Long id) {
+        employeeService.updateEmployee(id, employee);
     }
 }
--- src/main/java/com/codWithProject/employee/services/EmployeeService.java
+++ src/main/java/com/codWithProject/employee/services/EmployeeService.java
@@ -4,9 +4,9 @@ import com.codWithProject.employee.entity.Employee;
 import com.codWithProject.employee.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,14 +14,42 @@ public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
 
-
-    List<Employee> findAll(){
+    public List<Employee> getAllEmployee() {
         return employeeRepository.findAll();
     }
 
+    public Employee getEmployeeById(Long id){
+        return employeeRepository.findById(id).orElseThrow(null);
+    }
 
 
-    public Employee postEmployee(Employee employee){
+    public Employee postEmployee(Employee employee) {
         return employeeRepository.save(employee);
     }
+
+    public Optional<Employee> deleteEmployee(Long id) {
+        Optional<Employee> employeeOptional = employeeRepository.findById(id);
+        employeeOptional.ifPresent(employeeDb -> {
+            employeeRepository.delete(employeeDb);
+        });
+        return employeeOptional;
+    }
+
+    public Optional<Employee> updateEmployee(Long id, Employee employee) {
+        Optional<Employee> employeeOptional = employeeRepository.findById(id);
+        if(employeeOptional.isPresent()){
+            Employee employeetDb = employeeOptional.orElseThrow();
+            employeetDb.setName(employee.getName());
+            employeetDb.setEmail(employee.getEmail());
+            employeetDb.setPhone(employee.getPhone());
+            employeetDb.setDepartment(employee.getDepartment());
+
+            return Optional.of(employeeRepository.save(employeetDb));
+        }
+        return employeeOptional;
+    }
+
+
+
 }
+
